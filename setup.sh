41fbd3a4914ce1e3917358b94d9589d2fa087619
dginsburg94 <41930867+dginsburg94@users.noreply.github.com>
2020-01-09 14:54:33 -0500

    updated setup script to make service

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-ln -s ./SnortRuleGenerator.py /usr/local/bin
-ln -s ./SnortRuleGenerator /etc/init.d/SnortRuleGenerator
-ln -s ./SnortRuleGenerator /etc/rc5.d/S01SnortRuleGenerator
+current_dir=$(pwd)
+ln -s $current_dir/SnortRuleGenerator.py /usr/local/bin
+ln -s $current_dir/SnortRuleGenerator /etc/init.d/SnortRuleGenerator
+ln -s $current_dir/SnortRuleGenerator /etc/rc5.d/S01SnortRuleGenerator
 systemctl daemon-reload
 systemctl enable SnortRuleGenerator.service
 systemctl start SnortRuleGenerator
 systemctl status SnortRuleGenerator
 read -p 'Please enter the interface for packet capture: ' interface
 echo '#!/usr/bin/env python3' > interface.py
-echo 'interface = ${interface}' >> interface.py
+echo 'interface = ' $interface >> interface.py
+chmod +x *.py
+chmod +x SnortRuleGenerator
 
